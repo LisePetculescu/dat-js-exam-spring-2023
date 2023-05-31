6f6e8be1257b2070be3aefa70c4bff9bb76bbe19
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-05-31 10:17:40 +0200

    øvelse

--- environment-03/script.js
+++ environment-03/script.js
@@ -1,1 +1,43 @@
 "use strict";
+
+import { events } from "./data.js";
+
+window.addEventListener("load", start);
+
+function start() {
+    console.log(events);
+
+    showEvents();
+}
+
+function showEvents() {
+    document.querySelector("#students-list").innerHTML = "";
+
+    events.sort(sortByDate);
+
+    for (const event of events) {
+        console.log(event);
+        showEvent(event)
+    }
+}
+
+function showEvent(event) {
+    const html = /*html */ `
+    
+    <li>Title: ${event.title}</li>
+    <li>Description: ${event.description}</li>
+    <li>Date: ${event.date}</li>
+    <br>
+    `
+    document.querySelector("#students-list").insertAdjacentHTML("beforeend", html);
+}
+
+function sortByDate(a, b) {
+
+    const dateOne = new Date(a.date).getTime();
+    const dateTwo = new Date(b.date).getTime();
+
+    return dateOne - dateTwo;
+
+}
+
