f765656b875123a82399872ea65ddfebfb9a810d
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-06-07 21:36:00 +0200

    gg

--- environment-09/script.js
+++ environment-09/script.js
@@ -1,1 +1,46 @@
 "use strict";
+
+// 1. hent posts listen
+
+window.addEventListener("load", start);
+
+let posts = [];
+
+async function start() {
+  console.log("Hello World");
+
+  posts = await getPosts();
+  showPosts(posts);
+}
+
+async function getPosts() {
+  const response = await fetch("posts.json");
+  const data = await response.json();
+
+  console.log(data);
+  return data;
+}
+
+// 2. vis posts som er published
+
+function showPosts(posts) {
+  for (const post of posts) {
+    if (post.published) {
+      showPosts(post);
+    }
+  }
+}
+
+function showPost(post) {
+  const html = /* html */ `
+    <article>
+        <img src=${post.image}  />
+        <h2>${post.caption}</h2>
+        <p>Likes: ${post.likes}</p>
+    </article>
+    `;
+
+    document.querySelector("#posts-list").insertAdjacentHTML("beforeend", html);
+}
+
+// 3. vis alle posts når der bliver trykket hvis alle
